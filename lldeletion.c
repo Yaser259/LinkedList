65c84e080034570a90888331e066f45cd8166cd1
Yaser <62748315+YaserTheDev@users.noreply.github.com>
2021-05-12 20:16:04 +0530

    Update lldeletion.c

--- lldeletion.c
+++ lldeletion.c
@@ -1,1 +1,17 @@
+//DELETE THE NUMBER AT ANY POSITION
+void deleteatanypos(int n){
+    struct node* temp = head;
+    if(n==1){
+        head = temp->next;
+        free(temp);
+        return;
+    }
+    int i;
+    for(i=0;i<n-2;i++){
+        temp=temp->next;
+    }
+    struct node* temp2=temp->next;
+    temp->next=temp2->next;
+    free(temp2);
+}
 
